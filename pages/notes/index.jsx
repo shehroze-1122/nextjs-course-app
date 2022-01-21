4492f0f700ab726a5ed88a2e5d23286e697ce706
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2022-01-21 12:08:24 +0500

    deployment tweak

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -29,8 +29,8 @@ const notes = ({notes}) => {
 export default notes;
 
 export async function getServerSideProps() {
-    console.log(`${process.env.VERCEL_URL}/api/note/`)
-    const res = await fetch(`${process.env.VERCEL_URL}/api/note/`)
+
+  const res = await fetch(`${process.env.APP_URL}/api/note/`)
     const {data} = await res.json()
     return {
       props: {notes: data}
